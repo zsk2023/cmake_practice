5c9ac506ff1bd1df61fddea504a7b5abf623f59a
cmake_practice <139089552+zsk2023@users.noreply.github.com>
2023-07-20 11:29:43 +0800

    Update main.cpp

--- 11/main.cpp
+++ 11/main.cpp
@@ -12,7 +12,7 @@ void getpeoples(){
     people2.changePrices();
     people2.buy(2,5);
     people2.peopleInfo();
-    People people3("Ann",30,1111);
+    People people3("Ann",30,2222);
     people3.changePrices();
     people3.buy(1,5);
     people3.peopleInfo();
